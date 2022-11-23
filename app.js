cbeae41d4fd76003d55bf6f2e6180393562a3d5b
Anurag Kasudhan <114092146+Kasudhananurag@users.noreply.github.com>
2022-11-23 11:57:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+module.exports={
+    a:10,
+    b:20,
+    names: "anurag",
+    show : () => {
+        console.log("hey")
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("hello");
+console.warn("hye");
+
+const app = require('./app');
+const a = require('./app')
+console.log(app.a,app.b)
+app.show()
